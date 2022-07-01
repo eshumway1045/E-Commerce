be1347a426915e32d86cbc270a3036166458f738
Eric Shumway <ericshumway2@mail.weber.edu>
2022-07-01 09:36:40 -0400

    Code Cleanup and Changes made

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,54 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['product_name', 'price', 'stock'],
+    include: [
+      {
+      model: Tag,
+      as: 'tags',
+      attributes: ['tag_name']
+    },
+    {
+      model: Category,
+      attributes: ['category_name']
+    }
+  ]
+  })
+  .then(dbProductData => {
+    console.log(dbProductData);
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    attributes: ['product_name', 'price', 'stock'],
+    include: [
+      {
+      model: Tag,
+      as: 'tags',
+      attributes: ['tag_name']
+    },
+    {
+      model: Category,
+      attributes: ['category_name']
+    }
+  ]
+  })
+  .then(dbProductData => {
+    console.log(dbProductData);
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -91,6 +131,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbProductData)})
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
